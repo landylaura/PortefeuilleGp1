08f62cfa7432f4d1f5935e306e4b50ea8513ce27
Landy <Landy@10.12.107.233>
2019-02-12 12:30:35 +0100

    correction

--- src/ActionCompose.java
+++ src/ActionCompose.java
@@ -2,44 +2,46 @@
  * Classe heritee de la classe Action
  */
 import java.util.HashMap;
-//Ok guyys, this is my first comment 
-public class ActionCompose extends Action{
-	
-	private HashMap<ActionSimple, Part> table;
 
-	/**
-	 * Constructeur d'une action composee qui initie la structure qui contiendra pour une action simple donnee (cle) son pourcentage de detention (valeur)
+/**.
+ *
+ * @author Landy
+ */
+public class ActionCompose extends Action {
+
+        /**
+         * @param table : table des actions
+         */
+        private final HashMap<ActionSimple, Part> table;
+	/**.
+	 * Constructeur d'une action composee qui initie la structure
 	 * @param l : nom de l'action composee
 	 */
-	public ActionCompose(String l) {
+	public ActionCompose(final String l) {
 		super(l);
-		this.table = new HashMap<ActionSimple, Part>();
+		this.table = new HashMap<>();
 	}
-	
-	/**
+	/**.
 	 * Methode qui permet d'ajouter une action simple a une action composee
 	 * @param as : action simple
 	 * @param p : pourcentage de detention de l'action simple
 	 */
-	public void ajouterActionSimple(ActionSimple as, float p) {			
-		this.table.put(as, new Part(p)); 
+	public void ajouterActionSimple(ActionSimple as, float p) {
+		this.table.put(as, new Part(p));
 	}
-	
+
 	@Override
-	/**
+	/**.
 	 * Methode qui permet de recuperer la valeur d'une action composee
 	 */
 	public float getValeur(String date) {
-		float somme = 0 ;
-		
-		for (ActionSimple as  : table.keySet()){
+		float somme = 0;
+
+		for (ActionSimple as  : table.keySet()) {
 			somme = somme + (table.get(as).getPourcentage() * as.getValeur(date)) ;
 		}
-			
-		return somme ;
+
+		return somme;
 	}
-	 
-	 
-	
 
 }
